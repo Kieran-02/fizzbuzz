a4f4c74b56de62405321656a0fa9ed6db8968cf1
Kieran <kieran3000@protonmail.com>
2024-04-21 14:42:28 +0100

    trying diffrent logic

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -8,17 +8,17 @@ import (
 func main() {
 	for i := 1; i < 100; i++ {
 		checktext := ""
-		if i%3 == 0 {
-			checktext += "Fizz"
-		}
-		if i%5 == 0 {
-			checktext += "Buzz"
-		}
-		if i%3 != 0 && i%5 != 0 {
+		if i%3 == 0 || i%5 == 0 {
+			if i%3 == 0 {
+				checktext += "Fizz"
+			}
+			if i%5 == 0 {
+				checktext += "Buzz"
+			}
+		} else {
 			checktext += strconv.Itoa(i)
 		}
 		fmt.Println(checktext)
 	}
-
 	fmt.Println("TaDa!")
 }
